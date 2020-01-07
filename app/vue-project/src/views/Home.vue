96ee9c91c4d8c86bb439356c64798fd79bcb5b1e
矢野大樹 <daikitech0123@gmail.com>
2020-01-07 16:23:00 +0200

    remove unnecessary file

--- app/vue-project/src/views/Home.vue
+++ app/vue-project/src/views/Home.vue
@@ -1,23 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" v-on:click='consoleTest'>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Index.page
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
   },
   methods:{
-    consoleTest() {
-      console.log("ConsoleTest");
-    }
+    
   }
   
 }
